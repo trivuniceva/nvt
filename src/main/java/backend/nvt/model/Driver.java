0cf73ed09dcc9aa028cf883b87b34b61fc6fad1c
trivuniceva <trivunic99@gmail.com>
2024-08-20 05:59:03 +0200

    Update Driver.java

--- src/main/java/backend/nvt/model/Driver.java
+++ src/main/java/backend/nvt/model/Driver.java
@@ -1,14 +1,21 @@
 package backend.nvt.model;
 
+import jakarta.persistence.*;
+
 import java.util.List;
 
-public class Driver extends User{
+@Entity
+@DiscriminatorValue("DRIVER")
+public class Driver extends User {
 
+    @OneToMany(mappedBy = "driver", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Ride> rides;
 
+    @Column(nullable = false)
     private boolean available;
+
+    @Column(nullable = false)
     private int workingHours;
 
-
-
 }
+
