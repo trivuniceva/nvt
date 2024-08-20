0bc5e13c811dac116e53f249cee5822a8baf1ab7
trivuniceva <trivunic99@gmail.com>
2024-08-20 04:21:25 +0200

    svasta nesto

--- frontend/src/app/features/auth/auth-routing.module.ts
+++ frontend/src/app/features/auth/auth-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import {RidesComponent} from "../../shared/components/rides/rides.component";
+import {SignupComponent} from "./signup/signup.component";
+import {ForgottenPasswordComponent} from "./forgotten-password/forgotten-password.component";
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'password',
+    component: ForgottenPasswordComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
   },
   {
     path: 'ride',
--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,19 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  username: string = '';
+  email: string = '';
   password: string = '';
   errorMessage: string = '';
+
+  constructor(private router: Router) {
+  }
+
+  goToSignup(){
+    this.router.navigate(['/signup'])
+  }
+
+  forgotPassword(){
+    this.router.navigate(['/password'])
+  }
 
   onSubmit() {
     // Dodajte svoju logiku za prijavu ovde
     // Na primer, proverite korisničke podatke ili pošaljite ih serveru
-    if (this.username === '' || this.password === '') {
+    if (this.email === '' || this.password === '') {
       this.errorMessage = 'Please fill in both fields.';
     } else {
       // Pozovite servis za prijavu, itd.
       this.errorMessage = '';
-      console.log('Username:', this.username);
+      console.log('Email:', this.email);
       console.log('Password:', this.password);
     }
   }
