2c1eff3c8e8fd3d4596e3e338e05f185bf23cbe0
trivuniceva <trivunic99@gmail.com>
2024-08-20 05:51:36 +0200

    Create Driver.java

---
+++ src/main/java/backend/nvt/model/Driver.java
@@ -0,0 +1,14 @@
+package backend.nvt.model;
+
+import java.util.List;
+
+public class Driver extends User{
+
+    private List<Ride> rides;
+
+    private boolean available;
+    private int workingHours;
+
+
+
+}
