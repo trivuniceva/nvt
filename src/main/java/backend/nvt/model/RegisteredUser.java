dd5cfb2f609177ad6ff634f1431e77694b199d34
trivuniceva <trivunic99@gmail.com>
2024-08-20 05:59:04 +0200

    Update RegisteredUser.java

--- src/main/java/backend/nvt/model/RegisteredUser.java
+++ src/main/java/backend/nvt/model/RegisteredUser.java
@@ -1,7 +1,21 @@
 package backend.nvt.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@DiscriminatorValue("REGISTERED_USER")
 public class RegisteredUser extends User {
 
+    @OneToMany(mappedBy = "registeredUser", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Ride> rides;
 
+    @ElementCollection
+    @CollectionTable(name = "favorite_routes", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "route")
+    private Set<String> favoriteRoutes;
 
 }
+
