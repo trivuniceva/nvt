1647237c6b3036fc6a627051f97ea621a1855f6a
trivuniceva <trivunic99@gmail.com>
2024-08-20 05:59:05 +0200

    Update Ride.java

--- src/main/java/backend/nvt/model/Ride.java
+++ src/main/java/backend/nvt/model/Ride.java
@@ -1,19 +1,49 @@
 package backend.nvt.model;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "rides")
 public class Ride {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "nvt_users")
     private RegisteredUser registeredUser;
+
+    @ManyToOne
+    @JoinColumn(name = "driver_id")
     private Driver driver;
+
+    @Column(nullable = false)
     private String startLocation;
+
+    @Column(nullable = false)
     private String endLocation;
+
+    @ElementCollection
+    @CollectionTable(name = "ride_stops", joinColumns = @JoinColumn(name = "ride_id"))
+    @Column(name = "stop_location")
+    private List<String> stops;
+
+    @Column(nullable = false)
     private double price;
 
+    @Column(nullable = false)
     private LocalDateTime startTime;
+
+    @Column(nullable = false)
     private LocalDateTime endTime;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private RideStatus status;
+
+
 }
