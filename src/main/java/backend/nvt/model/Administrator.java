26b3fb8aff94984a80e91be025736a27fad687b4
trivuniceva <trivunic99@gmail.com>
2024-08-20 05:59:02 +0200

    Update Administrator.java

--- src/main/java/backend/nvt/model/Administrator.java
+++ src/main/java/backend/nvt/model/Administrator.java
@@ -1,5 +1,10 @@
 package backend.nvt.model;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("ADMINISTRATOR")
 public class Administrator extends User{
 
 
