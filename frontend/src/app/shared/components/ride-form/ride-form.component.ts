27384ad2437ae350ed1d89c0fcd873ce15e66041
trivuniceva <trivunic99@gmail.com>
2024-08-27 03:32:02 +0200

    Update ride-form.component.ts

--- frontend/src/app/shared/components/ride-form/ride-form.component.ts
+++ frontend/src/app/shared/components/ride-form/ride-form.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RideService } from '../../../core/services/ride/ride.service';
 
 @Component({
   selector: 'app-ride-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './ride-form.component.html',
-  styleUrl: './ride-form.component.css'
+  styleUrls: ['./ride-form.component.css']
 })
 export class RideFormComponent {
+  polaziste: string = '';
+  destinacija: string = '';
 
+  @Output() formSubmitted: EventEmitter<{ polaziste: string, destinacija: string }> = new EventEmitter();
+
+  constructor(private rideService: RideService) {}
+
+  submitForm() {
+    this.rideService.setPolaziste(this.polaziste);
+    this.rideService.setDestinacija(this.destinacija);
+
+    this.formSubmitted.emit({
+      polaziste: this.polaziste,
+      destinacija: this.destinacija
+    });
+  }
 }
