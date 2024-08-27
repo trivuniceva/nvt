d76e970ed4e9f41f740a93852a8308161afc18d1
trivuniceva <trivunic99@gmail.com>
2024-08-27 04:54:37 +0200

    Create advanced-form.component.ts

---
+++ frontend/src/app/features/ride/advanced-form/advanced-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advanced-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './advanced-form.component.html',
+  styleUrl: './advanced-form.component.css'
+})
+export class AdvancedFormComponent {
+
+}
