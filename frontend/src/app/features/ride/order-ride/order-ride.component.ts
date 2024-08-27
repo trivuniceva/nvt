94353e406ea9ed73e36386f3af3574fcf07af5fd
trivuniceva <trivunic99@gmail.com>
2024-08-27 03:31:52 +0200

    Update order-ride.component.ts

--- frontend/src/app/features/ride/order-ride/order-ride.component.ts
+++ frontend/src/app/features/ride/order-ride/order-ride.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {MapComponent} from "../../../shared/components/map/map.component";
 import {RideFormComponent} from "../../../shared/components/ride-form/ride-form.component";
+import {RideGeneralComponent} from "../ride-general/ride-general.component";
 
 @Component({
   selector: 'app-order-ride',
@@ -8,6 +9,7 @@ import {RideFormComponent} from "../../../shared/components/ride-form/ride-form.
   imports: [
     MapComponent,
     RideFormComponent,
+    RideGeneralComponent,
   ],
   templateUrl: './order-ride.component.html',
   styleUrl: './order-ride.component.css'
