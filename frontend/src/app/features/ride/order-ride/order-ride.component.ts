4f2729cb8cd056737bf7cbdce9409bdefb2a9c41
trivuniceva <trivunic99@gmail.com>
2024-08-27 04:54:38 +0200

    Update order-ride.component.ts

--- frontend/src/app/features/ride/order-ride/order-ride.component.ts
+++ frontend/src/app/features/ride/order-ride/order-ride.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {MapComponent} from "../../../shared/components/map/map.component";
 import {RideFormComponent} from "../../../shared/components/ride-form/ride-form.component";
 import {RideGeneralComponent} from "../ride-general/ride-general.component";
+import {NgIf} from "@angular/common";
+import {AdvancedFormComponent} from "../advanced-form/advanced-form.component";
 
 @Component({
   selector: 'app-order-ride',
@@ -10,10 +12,18 @@ import {RideGeneralComponent} from "../ride-general/ride-general.component";
     MapComponent,
     RideFormComponent,
     RideGeneralComponent,
+    NgIf,
+    AdvancedFormComponent,
   ],
   templateUrl: './order-ride.component.html',
   styleUrl: './order-ride.component.css'
 })
 export class OrderRideComponent {
+  showRideForm: boolean = true;
+
+
+  hideRideForm(){
+    this.showRideForm = false;
+  }
 
 }
