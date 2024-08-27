f41b02b46b44668dff125a444eebadb92ae36924
trivuniceva <trivunic99@gmail.com>
2024-08-27 04:54:36 +0200

    Create advanced-form.component.spec.ts

---
+++ frontend/src/app/features/ride/advanced-form/advanced-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdvancedFormComponent } from './advanced-form.component';
+
+describe('AdvancedFormComponent', () => {
+  let component: AdvancedFormComponent;
+  let fixture: ComponentFixture<AdvancedFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AdvancedFormComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AdvancedFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
