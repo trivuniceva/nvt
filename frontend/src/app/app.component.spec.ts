3958c301d8bf0cc1856e434998cf2b9458c4b71d
trivuniceva <trivunic99@gmail.com>
2024-09-08 14:00:40 +0200

    Update app.component.spec.ts

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -28,14 +28,14 @@ describe('AppComponent', () => {
   it('should have a title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Bolji uber');
+    expect(app.title).toEqual('');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Bolji uber');
+    expect(compiled.querySelector('h1')?.textContent).toContain('');
   });
 
 });
