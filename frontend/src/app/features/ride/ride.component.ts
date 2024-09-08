77e716060874b132c42cd18971ed30016200eb81
trivuniceva <trivunic99@gmail.com>
2024-09-08 14:43:43 +0200

    create ride component

---
+++ frontend/src/app/features/ride/ride.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ride',
+  standalone: true,
+  imports: [
+  ],
+
+
+  templateUrl: './ride.component.html',
+  styleUrl: './ride.component.css'
+})
+export class RideComponent {
+
+}
